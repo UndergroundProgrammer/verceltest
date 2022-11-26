4ff16ed4f29e1745556f57062a5e1003e232c366
UndergroundProgrammer <88035195+UndergroundProgrammer@users.noreply.github.com>
2022-11-26 10:01:22 +0500

    some changes

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.get("/home", (req, res) => {
 app.get("/", (req, res) => {
   res.send("Server is running!!!");
 });
-app.listen(3000, () => {
-  console.log(`server is running on port 3000`);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
 });
